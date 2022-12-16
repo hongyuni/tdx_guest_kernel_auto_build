8abbd7905a79174e1f7634e42d0d08a05b9e5eab
Hongyu Ning <hongyu.ning@intel.com>
2022-12-16 11:32:36 +0800

    TDX guest kernel auto build init commit
    implemented the work flow of auto clone/checkout latest tag/revise
    kconfig/auto compile/auto TD guest booting test
    
    current target kernel are https://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git
    
    Signed-off-by: Hongyu Ning <hongyu.ning@intel.com>

---
+++ qemu.tdx.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+echo $SCRIPT_DIR
+
+#TDX guest kernel image passed by argument
+KERNEL_IMAGE=$1
+#TDVF from edk2 upstream
+BIOS_IMAGE=/home/tdx_hy/OVMF.edk2-stable202211.fd
+#QEMU from github tdx-qemu dev repo
+QEMU_IMAGE=/home/tdx_hy/git_qemu_tdx_github/qemu-tdx/build/qemu-system-x86_64.tdx-upstream-wip-2022-11-16-v7.1
+#GUEST_IMAGE qcow2 file
+GUEST_IMAGE=/home/tdx_hy/td-guest-centos-stream-8.hongyu.extend.qcow2
+MEM=1G
+
+if [[ ! -f $QEMU_IMAGE ]]; then
+	echo "Qemu image $QEMU_IMAGE does not exist"
+	exit 1
+else
+	echo "Using Qemu binary $QEMU_IMAGE"
+fi
+
+
+if [[ ! -f $KERNEL_IMAGE ]]; then
+	echo "Guest kernel $KERNEL_IMAGE does not exist"
+	exit 1
+else
+	echo "Guest kernel is $KERNEL_IMAGE"
+fi
+
+TDX_SYSFS_FILE="/sys/module/kvm_intel/parameters/tdx"
+if [[ -f $TDX_SYSFS_FILE ]]; then
+	if [ "Y" != "$(cat $TDX_SYSFS_FILE)" ] ;then
+    		echo "Please set tdx kvm_intel params to Y"
+		exit 1
+	fi
+else
+	echo "tdx modules params does not exist, reload correct kvm"
+	exit 1
+fi
+
+
+
+$QEMU_IMAGE \
+	-accel kvm \
+	-no-reboot \
+	-name process=tdxvm_hy,debug-threads=on \
+	-cpu host,host-phys-bits,pmu=off \
+	-smp cpus=4,cores=4,threads=1,sockets=1 \
+	-m ${MEM} \
+	-object tdx-guest,id=tdx,debug=on,sept-ve-disable=on,quote-generation-service=vsock:2:4050 \
+  -object memory-backend-memfd-private,id=ram1,size=${MEM} \
+	-machine q35,kernel_irqchip=split,confidential-guest-support=tdx,memory-backend=ram1 \
+	-bios $BIOS_IMAGE \
+  -d guest_errors \
+	-nographic \
+	-vga none \
+	-device virtio-net-pci,netdev=mynet0,mac=00:16:3E:68:08:FF,romfile= \
+  -netdev user,id=mynet0,hostfwd=tcp::10028-:22,hostfwd=tcp::12028-:2375 \
+  -device vhost-vsock-pci,guest-cid=28 \
+	-chardev stdio,id=mux,mux=on,signal=off \
+  -device virtio-serial,romfile= \
+	-device virtconsole,chardev=mux \
+  -serial chardev:mux \
+	-monitor chardev:mux \
+	-drive file=${GUEST_IMAGE},if=virtio,format=qcow2 \
+	-kernel ${KERNEL_IMAGE} \
+	-append "root=/dev/vda3 ro console=hvc0 earlyprintk=ttyS0 ignore_loglevel debug earlyprintk l1tf=off initcall_debug log_buf_len=200M swiotlb=force tsc=reliable mce=off efi=noruntime efi=debug nokaslr" \
+	-monitor pty \
+  -monitor telnet:127.0.0.1:9028,server,nowait \
+  -no-hpet \
+  -nodefaults \
---
+++ tdx.linux_next.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/bash
+
+# DESCRIPTION #
+# a template to test mainline linux_next kernel as TD guest image
+##  step 0: create $KERNEL_PATH if not exists and git clone https://github.com/torvalds/linux.git
+##  step 1: git checkout to latest release tag $TAG from above branch
+##  step 2: ./scripts/config revise CONFIG_INTEL_TDX_GUEST=y and append tag info $TAG to CONFIG_LOCALVERSION
+##  step 3: compile kernel and cp it as bzImage.$TAG
+##  step 4: pass above kernel bzImage to td_guest_boot.sh for TD guest booting test
+# may need to extend step 4 to cover more guest kernel booting regression check?
+
+# variables
+KERNEL_PATH=$HOME/linux_next_mainline
+
+# common functions
+script_path() {
+  #TEMP_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
+  #TEMP_PATH="$(dirname $0)"
+  #SCRIPT_PATH="$(cd "$TEMP_PATH" && pwd)"
+  SCRIPT_PATH="$( cd "$( dirname "$0" )" && pwd )"
+  [ -z "$SCRIPT_PATH" ] && exit 1
+  echo "$SCRIPT_PATH"
+}
+
+clone() {
+  [ ! -d "${KERNEL_PATH}" ] && mkdir "${KERNEL_PATH}"
+  cd "${KERNEL_PATH}" || exit 1
+  echo "switched to ${KERNEL_PATH}"
+  #git clone https://github.com/torvalds/linux.git
+  git clone https://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git
+  cd -- || exit 1
+}
+
+checkout() {
+  cd "${KERNEL_PATH}"/linux-next || exit 1
+  git checkout master
+  git reset --hard origin/master
+  echo "start to checkout to latest tag..."
+  git fetch --all
+  TAG=$(git tag --list --sort=-creatordate | head -1)
+  git checkout "${TAG}"
+  TAG_VERIFY=$(git describe --tags --exact-match)
+  [ "${TAG}" = "${TAG_VERIFY}" ] || exit 1
+  echo "checkout to tag:'${TAG}' done..."
+  cd -- || exit 1
+}
+
+kconfig() {
+  cd "${KERNEL_PATH}"/linux-next || exit 1
+  cp "$SOURCE_PATH"/tdx.gold.config .config
+  make olddefconfig
+  ./scripts/config --enable CONFIG_INTEL_TDX_GUEST --enable CONFIG_VIRT_DRIVERS --enable CONFIG_TDX_GUEST_DRIVER
+  ./scripts/config --set-str CONFIG_LOCALVERSION -"$TAG"
+  yes "" | make config
+  grep -r "CONFIG_INTEL_TDX_GUEST=y" .config || exit 1
+  grep -r "CONFIG_TDX_GUEST_DRIVER=y" .config || exit 1
+  #./scripts/config --set-str CONFIG_LOCALVERSION -"$TAG"
+}
+
+compile() {
+  cd "${KERNEL_PATH}"/linux-next || exit 1
+  rm -rf arch/x86/boot/bzImage
+  grep -r "CONFIG_INTEL_TDX_GUEST=y" .config || exit 1
+  grep -r "CONFIG_TDX_GUEST_DRIVER=y" .config || exit 1
+  make ARCH=x86_64 CC="ccache gcc" -j"$(nproc)" -C ./
+  cp arch/x86/boot/bzImage arch/x86/boot/bzImage."$TAG"
+}
+
+set -x
+
+echo "step 0:"
+echo $PWD
+
+SOURCE_PATH=$(script_path)
+
+# Do the work
+# step 0
+[ ! -d "$KERNEL_PATH"/linux-next ] && clone
+
+echo "step 1:"
+echo $PWD
+
+# step 1
+checkout
+
+echo "step 2:"
+echo $PWD
+
+# step 2
+kconfig
+
+echo "step 3:"
+echo $PWD
+
+# step 3
+compile
+
+echo "step 4:"
+grep -r "CONFIG_INTEL_TDX_GUEST=y" .config || exit 1
+grep -r "CONFIG_TDX_GUEST_DRIVER=y" .config || exit 1
+
+KERNEL_IMAGE="$KERNEL_PATH"/linux-next/arch/x86/boot/bzImage."${TAG}"
+
+# step 4
+/"$SOURCE_PATH"/qemu.tdx.sh "$KERNEL_IMAGE"
