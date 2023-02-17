95a0da730ae2c9fd36cfbe30abb0e807fa8711f5
Hongyu Ning <hongyu.ning@intel.com>
2023-02-17 11:20:15 +0800

    revise DAY_INFO format and copy test log to apache web share folder
    Signed-off-by: Hongyu Ning <hongyu.ning@intel.com>

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 rm td_guest_linux_next.log
 ./tdx.linux_next.sh 2>&1 | tee td_guest_linux_next.log
-DAY_INFO=$(date +%y-%m-%d)
-cp td_guest_linux_next.log td_guest_linux_next.${DAY_INFO}.log
+DAY_INFO=$(date +20%y%m%d)
+cp td_guest_linux_next.log /var/www/html/tdx-linux-next/td_guest_linux_next.${DAY_INFO}.log
