b5e6cc05d50508a18801456050c23c09b8ba7c1b
root <root@spr.jf.intel.com>
2023-05-25 03:13:42 -0400

    update logging storage based on test logic change
    update logging storage to support all test log files based on test logic change
    
    Signed-off-by: Ning Hongyu <hongyu.ning@intel.com>

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-rm td_guest_linux_next.log
-./tdx.linux_next.sh 2>&1 | tee td_guest_linux_next.log
+rm -rf test_tdx.log
+rm -rf vm_*.log
+./tdx.linux_next.sh 2>&1 | tee test_tdx.log
 DAY_INFO=$(date +20%y%m%d)
-cp td_guest_linux_next.log /var/www/html/tdx-linux-next/td_guest_linux_next.${DAY_INFO}.log
+sleep 10
+cd /var/www/html/tdx-linux-next
+mkdir -p ${DAY_INFO}
+cd -
+cp test_tdx.log /var/www/html/tdx-linux-next/${DAY_INFO}/
+cp vm_*.log /var/www/html/tdx-linux-next/${DAY_INFO}/
